55adaa955d08540ffccfc41e388bce1c4082bfdf
Navraj Narula <navrajnarula@gmail.com>
2018-04-27 23:02:49 -0400

    attempt line labeling

--- label_gender_in_scenes.py
+++ label_gender_in_scenes.py
@@ -25,26 +25,52 @@ def getAll(dataset):
 			all_.append((row['Title'], row['Source'], row['File_path'], row['IMDb_id']))
 	return all_
 
-def get_boundaries_agarwal(all_movies, char_gender_mapping):
+def get_gender_labels_for_characters(all_movies, char_gender_mapping):
 
 	movie_gender_labels = []
+	start_points = []
+	start_end = []
+	dialogue_boundaries = []
+	script_content = []
+	relevant_lines = []
+
 	movies_without_gender_labels = []
+
 	for item in all_movies:
 		with open(item[2]) as fp:
 			try:
 				mapping = char_gender_mapping[item[2]]
 				contents = fp.readlines()
 				for idx, line in enumerate(contents):
+					script_content.append(line)
 					if line[0:2] == "C|":
 						for char in mapping:
 							for c in char:
 								if c in line:
-									movie_gender_labels.append((line,idx,char[0]))
+									movie_gender_labels.append((idx,char[0],line))
 
 			except KeyError:
 				movies_without_gender_labels.append(item[2])
 
-	return set(movie_gender_labels)
+	gender_labels = sorted(set(movie_gender_labels), key=movie_gender_labels.index)
+
+	for x in gender_labels:
+		start_points.append(x[0])
+
+	for s in range(len(start_points) - 1):
+		current_item, next_item = start_points[s], start_points[s + 1]
+		start_end.append((current_item, next_item-1))
+
+
+	for label in gender_labels:
+		for bound in start_end:
+			if bound[0] == label[0]:
+				relevant_lines.append([label[1]]+script_content[bound[0]:bound[1]])
+	
+	# for item in relevant_lines:
+	# 	print(relevant_lines)
+	# 	print()
+
 
 if __name__ == "__main__":
 
@@ -57,7 +83,7 @@ if __name__ == "__main__":
 	all_movies = getSampleSet(agarwal)
 	# all_movies = getAll(gorinski)
 
-	m = get_boundaries_agarwal(all_movies, a_char_gender)
+	m = get_gender_labels_for_characters(all_movies, a_char_gender)
 	print(m)
 
 	# for item in all_movies:
