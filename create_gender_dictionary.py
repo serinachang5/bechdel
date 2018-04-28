b43cc44eacbfb1c3c3209315e5304435ee145ba2
Navraj Narula <navrajnarula@gmail.com>
2018-04-27 20:03:17 -0400

    create gender/character dictionary for agarwal and gorinski

--- create_gender_dictionary.py
+++ create_gender_dictionary.py
@@ -1,13 +1,17 @@
-import csv
 import os
+import pickle
 
-def get_gender_by_movie():
+# iteratetes through movie_by_gender directory
+# and creates dictionary to store each character
+# name, its variants, and gender roles
 
-	all_files = os.listdir("data/movie_by_gender/agarwal")
+# params: folder path
+# returns: dictionary of movie and character info
+def get_gender_by_movie(path):
 
-	count = 0
+	all_files = os.listdir(path)
 
-	gender_in_movies_agarwal = {}
+	gender_in_movies = {}
 
 	for filename in all_files:
 
@@ -19,17 +23,13 @@ def get_gender_by_movie():
 		store_variants = []
 		concat = []
 
-
-		with open("data/movie_by_gender/agarwal/"+filename) as fp:
-			if count < 3:
-				contents = fp.readlines()
-				gender_in_movies_agarwal[contents[4][11:]] = []
-				for idx, line in enumerate(contents):
-					if line[0:5] == "Root:":
-						chars.append(idx)
-					char_info.append(line)
-			count += 1
-
+		with open(path+filename) as fp:
+			contents = fp.readlines()
+			gender_in_movies_agarwal[contents[4][11:]] = []
+			for idx, line in enumerate(contents):
+				if line[0:5] == "Root:":
+					chars.append(idx)
+				char_info.append(line)
 
 		for c in range(len(chars) - 1):
 			current_item, next_item = chars[c], chars[c+1]
@@ -37,7 +37,6 @@ def get_gender_by_movie():
 
 		for bound in start_end_char:
 			char_boundaries.append(char_info[bound[0]:bound[1]])
-
 
 		for idx, char in enumerate(char_boundaries):
 			for i, c in enumerate(char):
@@ -49,26 +48,25 @@ def get_gender_by_movie():
 				if "Variants:" in c:
 					if len(char[i:]) != 1:
 					 	store_variants.append(tuple(char[i+1:]))
-
 		
 		if len(store_variants) > 0 and len(store_gender) > 0:
 			for g, v in zip(store_gender, store_variants):
 				concat.append((g,) + v)
 				new_concat = [':'.join(x).replace(' ', '').replace('\n', '') for x in concat]
 			
-				gender_in_movies_agarwal[contents[4][11:]] = new_concat
+				gender_in_movies[contents[4][11:]] = new_concat
 
-	return gender_in_movies_agarwal
-			
-
-
+	return gender_in_movies
 
 if __name__ == "__main__":
 
-	agarwal_movie_genders = get_gender_by_movie()
-	print(agarwal_movie_genders)
+	agarwal_chars_by_gender = get_gender_by_movie("data/movie_by_gender/agarwal/")
+	gorinski_chars_by_gender = get_gender_by_movie("data/movie_by_gender/gorinski/")
 
+	# store agarwal dictionary in pickle file
+	with open("agarwal_chars_by_gender.pickle", 'wb') as handle:
+		pickle.dump(agarwal_chars_by_gender, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-
-
-
+	# store gorinski dictionary in pickle file
+	with open("gorinski_chars_by_gender.pickle", 'wb') as handle:
+		pickle.dump(gorinski_chars_by_gender, handle, protocol=pickle.HIGHEST_PROTOCOL)
