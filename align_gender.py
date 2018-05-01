fc838fd233128890ea2c15fd067f5fc430221590
Serina Chang <sc3003@columbia.edu>
2018-04-30 22:52:50 -0400

    Refactored, added test functions.

--- align_gender.py
+++ align_gender.py
@@ -1,7 +1,9 @@
+from get_scene_boundaries import get_boundaries_agarwal, get_boundaries_gorinski
 from csv import DictReader
 import os
 import pickle
 
+'''ALIGN GENDERS'''
 # return dict: year -> list of name_tuples
 def parse_SSA():
     path = './data/names/'
@@ -43,16 +45,56 @@ def get_name_scores(ssa_fname):
         name_to_counts[name] = score
     return name_to_counts
 
-# return dict: root -> [gen_score, variants]
-def classify_chars(screenplay_fname, movie_year, ssa_dict, source):
+# transform line into variant
+def line_to_variant(line, source):
+    assert(source == 'agarwal' or source == 'gorinski')
+    if source == 'agarwal':
+        var = line.split(None, 1)[1] # cut off C| + white space
+        var = var.strip()  # strip trailing white space
+    else:  # source == gorinski
+        var = line.strip()
+    return var
+
+# transform variant into root and return gender score if prefix found
+def variant_to_root(var):
     MALE_PREFIX = ['mr', 'mister', 'duke', 'father', 'brother', 'monsieur', 'prince', 'sir']
     FEMALE_PREFIX = ['ms', 'miss', 'mrs', 'duchess', 'mother', 'sister', 'mademoiselle', 'mlle',
                      'madame', 'princess']
     NEU_PREFIX = ['dr', 'doctor', 'gen', 'general', 'gov', 'governor', 'judge', 'lord', 'major',
                   'master', 'president', 'professor', 'prof', 'senator']
-    prefixes = [MALE_PREFIX, FEMALE_PREFIX, NEU_PREFIX]
     # https://mediawiki.middlebury.edu/wiki/LIS/Name_Standards
 
+    root = None
+    gen_score = None
+    toks = var.lower().split()
+    # if len(toks) > 0:
+        # if toks[0].strip('\'\".-:') == 'the': # remove leading 'the'
+            # toks.pop(0)
+    if len(toks) > 0:
+        first = toks[0].strip('\'\".-:')
+        if first in MALE_PREFIX:
+            if len(toks) > 1 and not toks[1].startswith('('):
+                root = first + ' ' + toks[1].strip('\'\".-:')
+            else:
+                root = first
+            gen_score = 0.0
+        elif first in FEMALE_PREFIX:
+            if len(toks) > 1 and not toks[1].startswith('('):
+                root = first + ' ' + toks[1].strip('\'\".-:')
+            else:
+                root = first
+            gen_score = 1.0
+        elif first in NEU_PREFIX:
+            if len(toks) > 1 and not toks[1].startswith('('):
+                root = first + ' ' + toks[1].strip('\'\".-:')
+            else:
+                root = first
+        else:
+            root = first
+    return root, gen_score
+
+# return dict: root -> [gen_score, variants]
+def classify_chars(screenplay_fname, movie_year, ssa_dict, source):
     decade_start = movie_year - 9
     ssa_name_scores = []
     for year in range(decade_start, movie_year+1):
@@ -61,9 +103,9 @@ def classify_chars(screenplay_fname, movie_year, ssa_dict, source):
 
     root_to_info = None
     if source == 'agarwal':
-        root_to_info = parse_agarwal_chars(screenplay_fname, prefixes)
+        root_to_info = parse_agarwal_chars(screenplay_fname)
     elif source == 'gorinski':
-        root_to_info = parse_gorinski_chars(screenplay_fname, prefixes)
+        root_to_info = parse_gorinski_chars(screenplay_fname)
 
     for root,(gen, vars) in root_to_info.items():
         if gen is None:
@@ -80,68 +122,30 @@ def classify_chars(screenplay_fname, movie_year, ssa_dict, source):
     return root_to_info
 
 # return dict: root -> gender, variants
-def parse_agarwal_chars(file_path, prefixes):
-    male_pre, fem_pre, neu_pre = prefixes
+def parse_agarwal_chars(file_path):
     with open(file_path, 'r') as f:
         root_to_info = {} # root mapped to gender score and variants
         for line in f.readlines():
             if line.startswith('C|'):
-                # process variant
-                var = line.split(None, 1)[1] # cut off C| + white space
+                var = line_to_variant(line, source='agarwal')
                 if var.startswith('('): # probably a description e.g. '(QUIETLY)'
                     continue
-                var = var.strip() # strip trailing white space
 
-                # process tokens
-                toks = var.lower().split()
-                if len(toks) > 0:
-                    if toks[0].strip('\'\".-:') == 'the': # remove leading 'the'
-                        toks.pop(0)
-                if len(toks) > 0:
-                    first = toks[0].strip('\'\".-:')
-                    if first in male_pre:
-                        if len(toks) > 1 and not toks[1].startswith('('):
-                            root = first + ' ' + toks[1].strip('\'\".-:')
-                        else:
-                            root = first
-                        if root in root_to_info:
-                            root_to_info[root][0] = 0.0
-                            root_to_info[root][1].add(var)
-                        else:
-                            root_to_info[root] = [0.0, {var}]
-                    elif first in fem_pre:
-                        if len(toks) > 1 and not toks[1].startswith('('):
-                            root = first + ' ' + toks[1].strip('\'\".-:')
-                        else:
-                            root = first
-                        if root in root_to_info:
-                            root_to_info[root][0] = 1.0
-                            root_to_info[root][1].add(var)
-                        else:
-                            root_to_info[root] = [1.0, {var}]
-                    elif first in neu_pre:
-                        if len(toks) > 1 and not toks[1].startswith('('):
-                            root = first + ' ' + toks[1].strip('\'\".-:')
-                        else:
-                            root = first
-                        if root in root_to_info:
-                            root_to_info[root][1].add(var)
-                        else:
-                            root_to_info[root] = [None, {var}]
-                    else:
-                        root = first
-                        if root in root_to_info:
-                            root_to_info[root][1].add(var)
-                        else:
-                            root_to_info[root] = [None, {var}]
-        return root_to_info
+                root, gen_score = variant_to_root(var)
+                if root not in root_to_info:
+                    root_to_info[root] = [None, set()]
+                if gen_score is not None:
+                    root_to_info[root][0] = gen_score
+                root_to_info[root][1].add(var)
 
-def parse_gorinski_chars(file_path, prefixes):
-    male_pre, fem_pre, neu_pre = prefixes
+    return root_to_info
+
+# return dict: root -> gender, variants
+def parse_gorinski_chars(file_path):
     with open(file_path, 'r') as f:
         max_ls = 0
         lines = f.readlines()
-        for line in lines[:100]: # find max leading space in first 100 lines
+        for line in lines[:100]:  # find max leading space in first 100 lines
             leading_space = len(line) - len(line.lstrip(' '))
             max_ls = max(leading_space, max_ls)
 
@@ -149,55 +153,20 @@ def parse_gorinski_chars(file_path, prefixes):
         for line in lines:
             leading_space = len(line) - len(line.lstrip(' '))
             if leading_space >= max_ls:
-                # process variant
-                var = line.strip() # strip leading and trailing white space
+                var = line_to_variant(line, source='gorinski')
                 if var.startswith('('): # probably a description e.g. '(QUIETLY)'
                     continue
 
-                # process tokens
-                toks = var.lower().split()
-                if len(toks) > 0:
-                    if toks[0].strip('\'\".-:') == 'the': # remove leading 'the'
-                        toks.pop(0)
-                if len(toks) > 0:
-                    first = toks[0].strip('\'\".-:')
-                    if first in male_pre:
-                        if len(toks) > 1 and not toks[1].startswith('('):
-                            root = first + ' ' + toks[1].strip('\'\".-:')
-                        else:
-                            root = first
-                        if root in root_to_info:
-                            root_to_info[root][0] = 0.0
-                            root_to_info[root][1].add(var)
-                        else:
-                            root_to_info[root] = [0.0, {var}]
-                    elif first in fem_pre:
-                        if len(toks) > 1 and not toks[1].startswith('('):
-                            root = first + ' ' + toks[1].strip('\'\".-:')
-                        else:
-                            root = first
-                        if root in root_to_info:
-                            root_to_info[root][0] = 1.0
-                            root_to_info[root][1].add(var)
-                        else:
-                            root_to_info[root] = [1.0, {var}]
-                    elif first in neu_pre:
-                        if len(toks) > 1 and not toks[1].startswith('('):
-                            root = first + ' ' + toks[1].strip('\'\".-:')
-                        else:
-                            root = first
-                        if root in root_to_info:
-                            root_to_info[root][1].add(var)
-                        else:
-                            root_to_info[root] = [None, {var}]
-                    else:
-                        root = first
-                        if root in root_to_info:
-                            root_to_info[root][1].add(var)
-                        else:
-                            root_to_info[root] = [None, {var}]
-        return root_to_info
+                root, gen_score = variant_to_root(var)
+                if root not in root_to_info:
+                    root_to_info[root] = [None, set()]
+                if gen_score is not None:
+                    root_to_info[root][0] = gen_score
+                root_to_info[root][1].add(var)
 
+    return root_to_info
+
+# write char_dict and other metadata to file
 def write_to_file(row, root_to_info, save_dir):
     file_name = '_'.join(row['Title'].split())
     file_name += '_' + row['Year'] + '.txt'
@@ -232,7 +201,45 @@ def write_to_file(row, root_to_info, save_dir):
 
     return found
 
+# returns title, year, imdb_id, bechdel, path, char_dict
+def parse_by_gender_file(path_to_file):
+    with open(path_to_file, 'r') as f:
+        lines = f.readlines()
+        title = lines[0].strip()
+        year = lines[1].strip().replace('Year: ', '')
+        imdb_id = lines[2].strip().replace('IMDb id: ', '')
+        bechdel = lines[3].strip().replace('Bechdel score: ', '')
+        path = lines[4].strip().replace('File path: ', '')
 
+        char_dict = {}
+        line_idx = 7  # 5 is \n, 6 is CHARACTERS
+        while line_idx < len(lines):
+            root = lines[line_idx].strip().replace('Root: ', '')
+            line_idx += 1
+            gender = lines[line_idx].strip().replace('Gender: ', '')
+            line_idx += 1
+            score = lines[line_idx].strip().replace('Score: ', '')
+            line_idx += 2 # skip Variants:
+            variants = []
+            while line_idx < len(lines) and not lines[line_idx].strip().startswith('Root: '):
+                variant = lines[line_idx].strip()
+                if len(variant) > 0:
+                    variants.append(variant)
+                line_idx += 1
+            char_dict[root] = [gender, score, variants]
+        info = [title, year, imdb_id, bechdel, path, char_dict]
+    return imdb_id, info
+
+
+'''PARSE SCENE'''''
+def get_variant_as_key(char_dict):
+    var2info = {}
+    for char,(gen, score, variants) in char_dict.items():
+        for v in variants:
+            var2info[v] = (char, gen, score)
+    return var2info
+
+# given a scene, return the list of char-dialogue tuples where char = (name, gender, score)
 def get_char_diag_list(scene, var2info, source):
     char_diag_list = []
 
@@ -240,14 +247,13 @@ def get_char_diag_list(scene, var2info, source):
         idx = 0
         while idx < len(scene):
             if scene[idx].startswith('C|'):
-                # process variant
-                var = scene[idx].split(None, 1)[1]  # cut off C| + white space
+                var = line_to_variant(scene[idx], source='agarwal')
                 if var.startswith('('):  # probably a description e.g. '(QUIETLY)'
                     idx += 1
                     continue
-                var = var.strip()  # strip trailing white space
+
                 if var in var2info:
-                    curr_char = var2info[var]  # root, gen, score
+                    curr_char = var2info[var]  # root, score
                     idx += 1
                     diag = []
                     while idx < len(scene) and scene[idx].startswith('D|'):
@@ -264,12 +270,12 @@ def get_char_diag_list(scene, var2info, source):
     else:  # source == 'gorinski'
         idx = 0
         while idx < len(scene):
-            possible = scene[idx].strip()
-            if possible.startswith('('):
+            var = line_to_variant(scene[idx], source='gorinski')
+            if var.startswith('('):
                 idx += 1
                 continue
-            if possible in var2info:
-                curr_char = var2info[possible]
+            if var in var2info:
+                curr_char = var2info[var]
                 idx += 1
                 if idx < len(scene):
                     diag = []
@@ -283,6 +289,7 @@ def get_char_diag_list(scene, var2info, source):
 
     return char_diag_list
 
+# given a char-dialogue list, return the continuous ff conversations
 def get_ff_conversations(char_diag_list):
     ffs = []
     prev_char = ''
@@ -311,22 +318,51 @@ def get_ff_conversations(char_diag_list):
             idx += 1
     return ffs
 
+def test_funcs_on_agarwal():
+    # 1. parse by_gender file
+    ten_things_by_gender = './movie_by_gender/agarwal/10_things_i_hate_about_you_1999.txt'
+    imdb_id, info = parse_by_gender_file(ten_things_by_gender)
+    # print(info)
+
+    # 2. set up
+    title, year, imdb_id, bechdel, path, char_dict = info
+    scenes = get_boundaries_agarwal(path)
+    # print(scenes[:5])
+    var_dict = get_variant_as_key(char_dict)
+    # print(var_dict)
+
+    # 3. parse scene
+    for sc in scenes[:1]:
+        print('SCENE')
+        print(sc)
+        cdl = get_char_diag_list(sc, var_dict, source='agarwal')
+        print('Char-Diag List')
+        for cd in cdl:
+            print(cd)
+        ffs = get_ff_conversations(cdl)
+        print('Fem-Fem dialogues')
+        for ff in ffs:
+            print(ff)
 
 if __name__ == "__main__":
-    SOURCE = 'agarwal'
+    # SOURCE = 'agarwal'
+    #
+    # ssa_dict = pickle.load(open('parsed_ssa.p', 'rb'))
+    # print('Number of years in SSA parsed:', len(ssa_dict))
+    #
+    # reader = DictReader(open('./data/' + SOURCE + '_alignments_with_IDs_with_bechdel.csv', 'r'))
+    # save_dir = './movie_by_gender/' + SOURCE + '/'
+    #
+    # count = 0
+    # for row in reader:
+    #     if row['Bechdel_rating'] != '':
+    #         count += 1
+    #         print('\n' + str(count), row['Title'].upper(), row['Year'])
+    #         root_to_info = classify_chars(row['File_path'], int(row['Year']), ssa_dict, source=SOURCE)
+    #         num_assigned = write_to_file(row, root_to_info, save_dir)
+    #         print('Number of root names:', len(root_to_info))
+    #         print('Number of gender matches made:', num_assigned)
+    test_funcs_on_agarwal()
 
-    ssa_dict = pickle.load(open('parsed_ssa.p', 'rb'))
-    print('Number of years in SSA parsed:', len(ssa_dict))
 
-    reader = DictReader(open('./data/' + SOURCE + '_alignments_with_IDs_with_bechdel.csv', 'r'))
-    save_dir = './movie_by_gender/' + SOURCE + '/'
 
-    count = 0
-    for row in reader:
-        if row['Bechdel_rating'] != '':
-            count += 1
-            print('\n' + str(count), row['Title'].upper(), row['Year'])
-            root_to_info = classify_chars(row['File_path'], int(row['Year']), ssa_dict, source=SOURCE)
-            num_assigned = write_to_file(row, root_to_info, save_dir)
-            print('Number of root names:', len(root_to_info))
-            print('Number of gender matches made:', num_assigned)
