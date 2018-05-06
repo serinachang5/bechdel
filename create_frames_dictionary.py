6d29ca65c2e45203368b9dc6dc1237a028d19d38
Navraj Narula <navrajnarula@gmail.com>
2018-05-05 20:36:00 -0400

    remake frames dictionary with stemmed verbs

--- create_frames_dictionary.py
+++ create_frames_dictionary.py
@@ -1,19 +1,26 @@
 import csv
 import pickle
 
+from nltk.stem.porter import PorterStemmer
+
 # iteratetes through agency/power verbs
-# and creates dictionary to store each verb
+# and creates dictionary to store each stemmed verb
 # and its associated agency and power
 
 def get_data(dataset):
+
 	all_ = []
 	for row in dataset:
-		all_.append((row["verb"], row["agency"], row["power"]))
+		all_.append([row["verb"], row["agency"], row["power"]])
 	return all_
 
 def create_frames_dict(frames):
+
+	stemmer = PorterStemmer()
 	frames_dict = {}
 	for f in frames:
+		# stem verbs
+		f[0] = stemmer.stem(f[0])
 		frames_dict[f[0]] = list(f[1:])
 	return frames_dict
 
@@ -27,4 +34,3 @@ if __name__ == "__main__":
 	# store frames dictionary in pickled file
 	with open("frames.pickle", 'wb') as handle:
 		pickle.dump(res, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
